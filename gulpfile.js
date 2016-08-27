acf1ccb33ca86f9b31ec7073050e6cb6c9d5930d
Ovi Savescu <savescu.ovidiu@gmail.com>
2016-08-27 17:41:15 +0300

    added browser-sync and source-maps
    added browser-sync and source-maps

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,45 @@
 var gulp  = require('gulp'),
     gutil = require('gulp-util');
     sass = require('gulp-sass');
+    browserSync = require('browser-sync').create(); // create a browser sync instance.
+    sourcemaps = require('gulp-sourcemaps');
 
 // create a default task and just log a message
 // gulp.task('default', function() {
 //   return gutil.log('Gulp is running!')
 // });
 
-gulp.task('build-css', function() {
-  return gulp.src('craft/templates/front-end-sources/scss/**/*.scss')
+var paths = {
+
+  styles: "craft/templates/front-end-sources/scss/**/*.scss",
+
+  source: "source"
+
+};
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        proxy: {
+    		target: "http://citadelswing.dev",
+		},
+		files: ["assets/css/*.css", "assets/js/*.js","craft/templates/**/*.html","craft/templates/**/*.inc"]
+        
+    });
+});
+
+// Compile styles and copy them to the assets css directory.
+gulp.task('styles', function () {
+  return gulp.src(paths.styles)
+    .pipe(sourcemaps.init())
     .pipe(sass())
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('assets/css'));
 });
 
-
 /* updated watch task to include sass */
+// gulp.task('watch', function() {
+//   gulp.watch('craft/templates/front-end-sources/scss/**/*.scss', ['styles']);
+// });
 
-gulp.task('watch', function() {
-  //gulp.watch('craft/templates/front-end-sources/js/**/*.js', ['jshint']);
-  gulp.watch('craft/templates/front-end-sources/scss/**/*.scss', ['build-css']);
+gulp.task('watch', [ 'browser-sync', 'styles' ], function() {
+  gulp.watch([ paths.styles ], [ 'styles']);
 });
